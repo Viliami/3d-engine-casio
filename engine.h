3786067949b5b47d6e7f7beeac10cd28d3a8c1d8
Viliami Tuanaki <Viliami@users.noreply.github.com>
2016-06-25 00:36:35 +1200

    Update engine.h

--- engine.h
+++ engine.h
@@ -16,8 +16,8 @@ class Engine{
             public:
                 string name;
                 Vector3 vertices[10];
-                Vector3 position[10];
-                Vector3 rotation[10];
+                Vector3 position;
+                Vector3 rotation;
                 Mesh(string name, int verticesCount);
         };
 };
