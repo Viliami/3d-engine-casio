4d3e512574b0e8d2747ec57fd0bbe6d48b4bc51c
Viliami Tuanaki <Viliami@users.noreply.github.com>
2016-06-25 22:26:18 +1200

    moved camera and mesh to seperate files

--- engine.cpp
+++ engine.cpp
@@ -1,13 +1,22 @@
 #include "engine.h"
-
-Engine::Engine(){
-    
+extern "C"{
+#include "fxlib.h"
+}
+Engine::Engine(int width, int height){
+    this->width = width;
+    this->height = height;
 }
 
-Engine::Camera::Camera(){
+extern "C"{
+    void Engine::clear(){
+        Bdisp_AllClr_VRAM();
+    }
     
-}
+    void Engine::present(){
+        Bdisp_PutDisp_DD();
+    }
+};
 
-Engine::Mesh::Mesh(string name, int verticesCount){
-    this->name = name;
+void Engine::render(Camera camera, Mesh meshes){
+    
 }
