bb52a3b46506cd0e5d434787887cc5eddac0349b
Viliami Tuanaki <Viliami@users.noreply.github.com>
2016-07-03 18:50:07 +1200

    changed matrix multiplication order
    matrix multiplication is not commutative, and euclid uses column major matrixes.

--- engine.cpp
+++ engine.cpp
@@ -1,9 +1,9 @@
 #include "engine.h"
 #define BLACK 1
 #define WHITE 0
-extern "C"{
+/*extern "C"{
 #include "fxlib.h"
-}
+}*/
 Engine::Engine(int width, int height){
     this->width = width;
     this->height = height;
@@ -93,78 +93,106 @@ void Engine::draw_point(Vector2 point){
 
 Vector2 Engine::project(Vector3 coord, Matrix4 transMat){
     Vector3 point = transMat.transform(coord);
-    double x = point.x * /*(this->width/64.0)*/ 1.5 + (this->width/2.0); 
-    double y = point.y * /*(this->height/64.0)*/ 1.5 + (this->height/2.0);
+    double x = point.x * (this->width) + (this->width/2.0); 
+    double y = point.y * (this->height) + (this->height/2.0);
     return Vector2(x, y); 
+}
+
+extern "C"{
+    
+    void save_stuff(const FONTCHARACTER *filename, int stuff_to_save){
+        int handle = Bfile_OpenFile(filename, _OPENMODE_WRITE);
+        if(handle <= 0){
+            Bfile_CreateFile(filename, sizeof(stuff_to_save));
+            Bfile_OpenFile(filename, _OPENMODE_WRITE);
+        }
+        Bfile_WriteFile(handle, &stuff_to_save, sizeof(stuff_to_save));
+    }
+    
+    Mesh Engine::load_obj(const FONTCHARACTER *filename){
+        const unsigned short * stuff=(const unsigned short *)"¥¥crd0¥¥filename.txt";
+        Mesh mesh = Mesh();
+        Vector3 vertices[600];
+        Vector3 faces[600];
+        int file_handle = Bfile_OpenFile(filename, _OPENMODE_READ);
+        if(file_handle <= 0){
+            return 0;
+        }
+        char* buffer;
+        Bfile_ReadFile(file_handle, &buffer, sizeof("# Blender3D"), 0);
+        return mesh;
+    }
 }
 
 void Engine::render(Camera camera, Mesh meshes[]){
     Matrix4 viewMatrix = Matrix4().new_look_at(camera.position, camera.target, Vector3(0,1,0));
-    Matrix4 projectionMatrix = Matrix4().new_perspective(0.2, 1, 0.01, 1.0);
+    Matrix4 projectionMatrix = Matrix4().new_perspective(0.78, (this->width/this->height), 0.01, 1.0);
     // for(int i = 0; i <= sizeof(meshes)/sizeof(meshes[0]); i++){
     for(int i = 0; i < 1; i++){
         Mesh mesh = meshes[i];
         Vector3 mRotation = mesh.rotation;
-        Matrix4 worldMatrix = Matrix4().new_rotate_euler(mRotation.y, mRotation.x, mRotation.z);
+        Matrix4 worldMatrix = Matrix4().new_rotate_euler(mRotation.y, mRotation.z, mRotation.x);
         Matrix4 translated = Matrix4().new_translate(mesh.position.x, mesh.position.y, mesh.position.z);
         //worldMatrix = worldMatrix * translated;
         Matrix4 result;
-        result.a = worldMatrix.a * translated.a + worldMatrix.b * translated.e + worldMatrix.c * translated.i + worldMatrix.d * translated.m;
-        result.b = worldMatrix.a * translated.b + worldMatrix.b * translated.f + worldMatrix.c * translated.j + worldMatrix.d * translated.n;
-        result.c = worldMatrix.a * translated.c + worldMatrix.b * translated.g + worldMatrix.c * translated.k + worldMatrix.d * translated.o;
-        result.d = worldMatrix.a * translated.d + worldMatrix.b * translated.h + worldMatrix.c * translated.l + worldMatrix.d * translated.p;
-        result.e = worldMatrix.e * translated.a + worldMatrix.f * translated.e + worldMatrix.g * translated.i + worldMatrix.h * translated.m;
-        result.f = worldMatrix.e * translated.b + worldMatrix.f * translated.f + worldMatrix.g * translated.j + worldMatrix.h * translated.n;
-        result.g = worldMatrix.e * translated.c + worldMatrix.f * translated.g + worldMatrix.g * translated.k + worldMatrix.h * translated.o;
-        result.h = worldMatrix.e * translated.d + worldMatrix.f * translated.h + worldMatrix.g * translated.l + worldMatrix.h * translated.p;
-        result.i = worldMatrix.i * translated.a + worldMatrix.j * translated.e + worldMatrix.k * translated.i + worldMatrix.l * translated.m;
-        result.j = worldMatrix.i * translated.b + worldMatrix.j * translated.f + worldMatrix.k * translated.j + worldMatrix.l * translated.n;
-        result.k = worldMatrix.i * translated.c + worldMatrix.j * translated.g + worldMatrix.k * translated.k + worldMatrix.l * translated.o;
-        result.l = worldMatrix.i * translated.d + worldMatrix.j * translated.h + worldMatrix.k * translated.l + worldMatrix.l * translated.p;
-        result.m = worldMatrix.m * translated.a + worldMatrix.n * translated.e + worldMatrix.o * translated.i + worldMatrix.p * translated.m;
-        result.n = worldMatrix.m * translated.b + worldMatrix.n * translated.f + worldMatrix.o * translated.j + worldMatrix.p * translated.n;
-        result.o = worldMatrix.m * translated.c + worldMatrix.n * translated.g + worldMatrix.o * translated.k + worldMatrix.p * translated.o;
-        result.p = worldMatrix.m * translated.d + worldMatrix.n * translated.h + worldMatrix.o * translated.l + worldMatrix.p * translated.p;
+        result.a = translated.a * worldMatrix.a + translated.b * worldMatrix.e + translated.c * worldMatrix.i + translated.d * worldMatrix.m;
+        result.b = translated.a * worldMatrix.b + translated.b * worldMatrix.f + translated.c * worldMatrix.j + translated.d * worldMatrix.n;
+        result.c = translated.a * worldMatrix.c + translated.b * worldMatrix.g + translated.c * worldMatrix.k + translated.d * worldMatrix.o;
+        result.d = translated.a * worldMatrix.d + translated.b * worldMatrix.h + translated.c * worldMatrix.l + translated.d * worldMatrix.p;
+        result.e = translated.e * worldMatrix.a + translated.f * worldMatrix.e + translated.g * worldMatrix.i + translated.h * worldMatrix.m;
+        result.f = translated.e * worldMatrix.b + translated.f * worldMatrix.f + translated.g * worldMatrix.j + translated.h * worldMatrix.n;
+        result.g = translated.e * worldMatrix.c + translated.f * worldMatrix.g + translated.g * worldMatrix.k + translated.h * worldMatrix.o;
+        result.h = translated.e * worldMatrix.d + translated.f * worldMatrix.h + translated.g * worldMatrix.l + translated.h * worldMatrix.p;
+        result.i = translated.i * worldMatrix.a + translated.j * worldMatrix.e + translated.k * worldMatrix.i + translated.l * worldMatrix.m;
+        result.j = translated.i * worldMatrix.b + translated.j * worldMatrix.f + translated.k * worldMatrix.j + translated.l * worldMatrix.n;
+        result.k = translated.i * worldMatrix.c + translated.j * worldMatrix.g + translated.k * worldMatrix.k + translated.l * worldMatrix.o;
+        result.l = translated.i * worldMatrix.d + translated.j * worldMatrix.h + translated.k * worldMatrix.l + translated.l * worldMatrix.p;
+        result.m = translated.m * worldMatrix.a + translated.n * worldMatrix.e + translated.o * worldMatrix.i + translated.p * worldMatrix.m;
+        result.n = translated.m * worldMatrix.b + translated.n * worldMatrix.f + translated.o * worldMatrix.j + translated.p * worldMatrix.n;
+        result.o = translated.m * worldMatrix.c + translated.n * worldMatrix.g + translated.o * worldMatrix.k + translated.p * worldMatrix.o;
+        result.p = translated.m * worldMatrix.d + translated.n * worldMatrix.h + translated.o * worldMatrix.l + translated.p * worldMatrix.p;
         
         worldMatrix = result;
         
         Matrix4 transformMatrix, result1, result2;
+        //worldMatrix * viewMatrix * projectionMatrix
         
-        result1.a = worldMatrix.a * viewMatrix.a + worldMatrix.b * viewMatrix.e + worldMatrix.c * viewMatrix.i + worldMatrix.d * viewMatrix.m;
-        result1.b = worldMatrix.a * viewMatrix.b + worldMatrix.b * viewMatrix.f + worldMatrix.c * viewMatrix.j + worldMatrix.d * viewMatrix.n;
-        result1.c = worldMatrix.a * viewMatrix.c + worldMatrix.b * viewMatrix.g + worldMatrix.c * viewMatrix.k + worldMatrix.d * viewMatrix.o;
-        result1.d = worldMatrix.a * viewMatrix.d + worldMatrix.b * viewMatrix.h + worldMatrix.c * viewMatrix.l + worldMatrix.d * viewMatrix.p;
-        result1.e = worldMatrix.e * viewMatrix.a + worldMatrix.f * viewMatrix.e + worldMatrix.g * viewMatrix.i + worldMatrix.h * viewMatrix.m;
-        result1.f = worldMatrix.e * viewMatrix.b + worldMatrix.f * viewMatrix.f + worldMatrix.g * viewMatrix.j + worldMatrix.h * viewMatrix.n;
-        result1.g = worldMatrix.e * viewMatrix.c + worldMatrix.f * viewMatrix.g + worldMatrix.g * viewMatrix.k + worldMatrix.h * viewMatrix.o;
-        result1.h = worldMatrix.e * viewMatrix.d + worldMatrix.f * viewMatrix.h + worldMatrix.g * viewMatrix.l + worldMatrix.h * viewMatrix.p;
-        result1.i = worldMatrix.i * viewMatrix.a + worldMatrix.j * viewMatrix.e + worldMatrix.k * viewMatrix.i + worldMatrix.l * viewMatrix.m;
-        result1.j = worldMatrix.i * viewMatrix.b + worldMatrix.j * viewMatrix.f + worldMatrix.k * viewMatrix.j + worldMatrix.l * viewMatrix.n;
-        result1.k = worldMatrix.i * viewMatrix.c + worldMatrix.j * viewMatrix.g + worldMatrix.k * viewMatrix.k + worldMatrix.l * viewMatrix.o;
-        result1.l = worldMatrix.i * viewMatrix.d + worldMatrix.j * viewMatrix.h + worldMatrix.k * viewMatrix.l + worldMatrix.l * viewMatrix.p;
-        result1.m = worldMatrix.m * viewMatrix.a + worldMatrix.n * viewMatrix.e + worldMatrix.o * viewMatrix.i + worldMatrix.p * viewMatrix.m;
-        result1.n = worldMatrix.m * viewMatrix.b + worldMatrix.n * viewMatrix.f + worldMatrix.o * viewMatrix.j + worldMatrix.p * viewMatrix.n;
-        result1.o = worldMatrix.m * viewMatrix.c + worldMatrix.n * viewMatrix.g + worldMatrix.o * viewMatrix.k + worldMatrix.p * viewMatrix.o;
-        result1.p = worldMatrix.m * viewMatrix.d + worldMatrix.n * viewMatrix.h + worldMatrix.o * viewMatrix.l + worldMatrix.p * viewMatrix.p;
+        //projection * viewMatrix * worldMatrix
+        result1.a = projectionMatrix.a * viewMatrix.a + projectionMatrix.b * viewMatrix.e + projectionMatrix.c * viewMatrix.i + projectionMatrix.d * viewMatrix.m;
+        result1.b = projectionMatrix.a * viewMatrix.b + projectionMatrix.b * viewMatrix.f + projectionMatrix.c * viewMatrix.j + projectionMatrix.d * viewMatrix.n;
+        result1.c = projectionMatrix.a * viewMatrix.c + projectionMatrix.b * viewMatrix.g + projectionMatrix.c * viewMatrix.k + projectionMatrix.d * viewMatrix.o;
+        result1.d = projectionMatrix.a * viewMatrix.d + projectionMatrix.b * viewMatrix.h + projectionMatrix.c * viewMatrix.l + projectionMatrix.d * viewMatrix.p;
+        result1.e = projectionMatrix.e * viewMatrix.a + projectionMatrix.f * viewMatrix.e + projectionMatrix.g * viewMatrix.i + projectionMatrix.h * viewMatrix.m;
+        result1.f = projectionMatrix.e * viewMatrix.b + projectionMatrix.f * viewMatrix.f + projectionMatrix.g * viewMatrix.j + projectionMatrix.h * viewMatrix.n;
+        result1.g = projectionMatrix.e * viewMatrix.c + projectionMatrix.f * viewMatrix.g + projectionMatrix.g * viewMatrix.k + projectionMatrix.h * viewMatrix.o;
+        result1.h = projectionMatrix.e * viewMatrix.d + projectionMatrix.f * viewMatrix.h + projectionMatrix.g * viewMatrix.l + projectionMatrix.h * viewMatrix.p;
+        result1.i = projectionMatrix.i * viewMatrix.a + projectionMatrix.j * viewMatrix.e + projectionMatrix.k * viewMatrix.i + projectionMatrix.l * viewMatrix.m;
+        result1.j = projectionMatrix.i * viewMatrix.b + projectionMatrix.j * viewMatrix.f + projectionMatrix.k * viewMatrix.j + projectionMatrix.l * viewMatrix.n;
+        result1.k = projectionMatrix.i * viewMatrix.c + projectionMatrix.j * viewMatrix.g + projectionMatrix.k * viewMatrix.k + projectionMatrix.l * viewMatrix.o;
+        result1.l = projectionMatrix.i * viewMatrix.d + projectionMatrix.j * viewMatrix.h + projectionMatrix.k * viewMatrix.l + projectionMatrix.l * viewMatrix.p;
+        result1.m = projectionMatrix.m * viewMatrix.a + projectionMatrix.n * viewMatrix.e + projectionMatrix.o * viewMatrix.i + projectionMatrix.p * viewMatrix.m;
+        result1.n = projectionMatrix.m * viewMatrix.b + projectionMatrix.n * viewMatrix.f + projectionMatrix.o * viewMatrix.j + projectionMatrix.p * viewMatrix.n;
+        result1.o = projectionMatrix.m * viewMatrix.c + projectionMatrix.n * viewMatrix.g + projectionMatrix.o * viewMatrix.k + projectionMatrix.p * viewMatrix.o;
+        result1.p = projectionMatrix.m * viewMatrix.d + projectionMatrix.n * viewMatrix.h + projectionMatrix.o * viewMatrix.l + projectionMatrix.p * viewMatrix.p;
 
         //transformMatrix = transformMatrix * projectionMatrix;
         
-        result2.a = result1.a * projectionMatrix.a + result1.b * projectionMatrix.e + result1.c * projectionMatrix.i + result1.d * projectionMatrix.m;
-        result2.b = result1.a * projectionMatrix.b + result1.b * projectionMatrix.f + result1.c * projectionMatrix.j + result1.d * projectionMatrix.n;
-        result2.c = result1.a * projectionMatrix.c + result1.b * projectionMatrix.g + result1.c * projectionMatrix.k + result1.d * projectionMatrix.o;
-        result2.d = result1.a * projectionMatrix.d + result1.b * projectionMatrix.h + result1.c * projectionMatrix.l + result1.d * projectionMatrix.p;
-        result2.e = result1.e * projectionMatrix.a + result1.f * projectionMatrix.e + result1.g * projectionMatrix.i + result1.h * projectionMatrix.m;
-        result2.f = result1.e * projectionMatrix.b + result1.f * projectionMatrix.f + result1.g * projectionMatrix.j + result1.h * projectionMatrix.n;
-        result2.g = result1.e * projectionMatrix.c + result1.f * projectionMatrix.g + result1.g * projectionMatrix.k + result1.h * projectionMatrix.o;
-        result2.h = result1.e * projectionMatrix.d + result1.f * projectionMatrix.h + result1.g * projectionMatrix.l + result1.h * projectionMatrix.p;
-        result2.i = result1.i * projectionMatrix.a + result1.j * projectionMatrix.e + result1.k * projectionMatrix.i + result1.l * projectionMatrix.m;
-        result2.j = result1.i * projectionMatrix.b + result1.j * projectionMatrix.f + result1.k * projectionMatrix.j + result1.l * projectionMatrix.n;
-        result2.k = result1.i * projectionMatrix.c + result1.j * projectionMatrix.g + result1.k * projectionMatrix.k + result1.l * projectionMatrix.o;
-        result2.l = result1.i * projectionMatrix.d + result1.j * projectionMatrix.h + result1.k * projectionMatrix.l + result1.l * projectionMatrix.p;
-        result2.m = result1.m * projectionMatrix.a + result1.n * projectionMatrix.e + result1.o * projectionMatrix.i + result1.p * projectionMatrix.m;
-        result2.n = result1.m * projectionMatrix.b + result1.n * projectionMatrix.f + result1.o * projectionMatrix.j + result1.p * projectionMatrix.n;
-        result2.o = result1.m * projectionMatrix.c + result1.n * projectionMatrix.g + result1.o * projectionMatrix.k + result1.p * projectionMatrix.o;
-        result2.p = result1.m * projectionMatrix.d + result1.n * projectionMatrix.h + result1.o * projectionMatrix.l + result1.p * projectionMatrix.p;
+        result2.a = result1.a * worldMatrix.a + result1.b * worldMatrix.e + result1.c * worldMatrix.i + result1.d * worldMatrix.m;
+        result2.b = result1.a * worldMatrix.b + result1.b * worldMatrix.f + result1.c * worldMatrix.j + result1.d * worldMatrix.n;
+        result2.c = result1.a * worldMatrix.c + result1.b * worldMatrix.g + result1.c * worldMatrix.k + result1.d * worldMatrix.o;
+        result2.d = result1.a * worldMatrix.d + result1.b * worldMatrix.h + result1.c * worldMatrix.l + result1.d * worldMatrix.p;
+        result2.e = result1.e * worldMatrix.a + result1.f * worldMatrix.e + result1.g * worldMatrix.i + result1.h * worldMatrix.m;
+        result2.f = result1.e * worldMatrix.b + result1.f * worldMatrix.f + result1.g * worldMatrix.j + result1.h * worldMatrix.n;
+        result2.g = result1.e * worldMatrix.c + result1.f * worldMatrix.g + result1.g * worldMatrix.k + result1.h * worldMatrix.o;
+        result2.h = result1.e * worldMatrix.d + result1.f * worldMatrix.h + result1.g * worldMatrix.l + result1.h * worldMatrix.p;
+        result2.i = result1.i * worldMatrix.a + result1.j * worldMatrix.e + result1.k * worldMatrix.i + result1.l * worldMatrix.m;
+        result2.j = result1.i * worldMatrix.b + result1.j * worldMatrix.f + result1.k * worldMatrix.j + result1.l * worldMatrix.n;
+        result2.k = result1.i * worldMatrix.c + result1.j * worldMatrix.g + result1.k * worldMatrix.k + result1.l * worldMatrix.o;
+        result2.l = result1.i * worldMatrix.d + result1.j * worldMatrix.h + result1.k * worldMatrix.l + result1.l * worldMatrix.p;
+        result2.m = result1.m * worldMatrix.a + result1.n * worldMatrix.e + result1.o * worldMatrix.i + result1.p * worldMatrix.m;
+        result2.n = result1.m * worldMatrix.b + result1.n * worldMatrix.f + result1.o * worldMatrix.j + result1.p * worldMatrix.n;
+        result2.o = result1.m * worldMatrix.c + result1.n * worldMatrix.g + result1.o * worldMatrix.k + result1.p * worldMatrix.o;
+        result2.p = result1.m * worldMatrix.d + result1.n * worldMatrix.h + result1.o * worldMatrix.l + result1.p * worldMatrix.p;
 
         transformMatrix = result2;
         //for(int j = 0; j <= sizeof(mesh.vertices)/sizeof(mesh.vertices[0]); j++){
