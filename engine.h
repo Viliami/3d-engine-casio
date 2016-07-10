945c4c45fe505b95f128b41916ffe20ae88265b2
Viliami Tuanaki <Viliami@users.noreply.github.com>
2016-07-10 12:18:52 +1200

    updated some functions

--- engine.h
+++ engine.h
@@ -13,9 +13,11 @@ class Engine{
         void present();
         void put_pixel(int x, int y, int color);
         void draw_line(int x1, int y1, int x2, int y2, int color);
+        void draw_thick_line(int x0, int y0, int x1, int y1, int thickness, int color);
+        void draw_rect(int x, int y, int w, int h, int color);
         Vector2 project(Vector3 coord, Matrix4 transMat);
         void draw_point(Vector2 point);
-        void render(Camera camera, Mesh meshes[]);
+        void render(Camera camera, Mesh* meshes[], int numMeshes = 1);
         Mesh load_obj(const FONTCHARACTER *filename);
         
 };
