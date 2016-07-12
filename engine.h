1c38c28bb3794f444fcc2cabd9efb2ba4ffe0ec7
Viliami Tuanaki <Viliami@users.noreply.github.com>
2016-07-13 11:11:42 +1200

    added fill_triangle function

--- engine.h
+++ engine.h
@@ -16,6 +16,8 @@ class Engine{
         void draw_line(int x1, int y1, int x2, int y2, int color);
         void draw_thick_line(int x0, int y0, int x1, int y1, int thickness, int color);
         void draw_rect(int x, int y, int w, int h, int color);
+        void processScanLine(int y, Vector2 va, Vector2 vb, Vector2 vc, Vector2 vd, int color);
+        void fill_triangle(Vector2 v0, Vector2 v1, Vector2 v2, int color);
         Vector2 project(Vector3 coord, Matrix4 transMat);
         void draw_point(Vector2 point);
         void render(Camera camera, Mesh* meshes[], int numMeshes = 1);
